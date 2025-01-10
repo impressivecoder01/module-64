305bb3ae2dd8b13b897c30ee1e158bc7edaa7e33
impressivecoder01 <rohanrahman251@gmail.com>
2025-01-11 01:56:37 +0600

    custom hook done

--- src/Pages/Home/PopularMenu/PopularMenu.jsx
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,17 +1,20 @@
 import React, { useEffect, useState } from "react";
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
 import MenuItem from "../../Shared/MenuItem/MenuItem";
+import useMenu from "../../../hooks/useMenu";
 
 const PopularMenu = () => {
-  const [menu, setMenu] = useState([]);
-  useEffect(() => {
-    fetch("menu.json")
-      .then((res) => res.json())
-      .then((data) => {
-        const popularItems = data.filter((item) => item.category === "popular");
-        setMenu(popularItems);
-      });
-  }, []);
+  // const [menu, setMenu] = useState([]);
+  // useEffect(() => {
+  //   fetch("menu.json")
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       const popularItems = data.filter((item) => item.category === "popular");
+  //       setMenu(popularItems);
+  //     });
+  // }, []);
+  const [menu] = useMenu();
+  const popular = menu.filter(item => item.category === 'popular')
   
   return (
     <div>
@@ -21,7 +24,7 @@ const PopularMenu = () => {
           subHeading={"Popular Items"}
         ></SectionTitle>
         <div className="grid md:grid-cols-2 gap-3">
-          {menu.map((item) => (
+          {popular.map((item) => (
             <MenuItem key={item._id} item={item}></MenuItem>
           ))}
         </div>
--- src/Pages/Menu/Menu.jsx
+++ src/Pages/Menu/Menu.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
-
+import Cover from '../Shared/Cover/Cover';
+import menuImg from '../../../src/assets/menu/banner3.jpg'
+import PopularMenu from '../Home/PopularMenu/PopularMenu';
 const Menu = () => {
     return (
         <div>
             <Helmet>
                 <title>Bistro | Menu</title>
             </Helmet>
-            <h1>menu</h1>
+            <Cover img={menuImg} title={'our menu'}></Cover>
+            <PopularMenu></PopularMenu>
         </div>
     );
 };
