28a27c5baf4de574bf1c7dfc894f80e2c445b9c3
impressivecoder01 <rohanrahman251@gmail.com>
2025-01-11 02:50:30 +0600

    using custom hook

--- src/Pages/Menu/Menu.jsx
+++ src/Pages/Menu/Menu.jsx
@@ -3,14 +3,24 @@ import { Helmet } from 'react-helmet-async';
 import Cover from '../Shared/Cover/Cover';
 import menuImg from '../../../src/assets/menu/banner3.jpg'
 import PopularMenu from '../Home/PopularMenu/PopularMenu';
+import useMenu from '../../hooks/useMenu';
+import SectionTitle from '../../Components/SectionTitle/SectionTitle';
+import MenuCategory from './MenuCategory/MenuCategory';
 const Menu = () => {
+    const [menu] = useMenu();
+    const dessert = menu.filter(item => item.category === 'dessert')
+    const pizza = menu.filter(item => item.category === 'pizza')
+    const salad = menu.filter(item => item.category === 'salad')
+    const soup = menu.filter(item => item.category === 'soup')
+    const offered = menu.filter(item => item.category === 'offered')
     return (
         <div>
             <Helmet>
                 <title>Bistro | Menu</title>
             </Helmet>
             <Cover img={menuImg} title={'our menu'}></Cover>
-            <PopularMenu></PopularMenu>
+            <SectionTitle subHeading={'Don"t miss '} heading={"Today's Offer"}></SectionTitle>
+            <MenuCategory items={offered}></MenuCategory>
         </div>
     );
 };
